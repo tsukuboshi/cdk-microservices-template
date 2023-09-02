08bc3895e841a42bf038271a87151a4b604c6f55
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2023-09-02 23:26:40 +0900

    first-commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# cdk-microservices-template
+
+## Overview
+
+This repository contains cdk template for microservices architecture.
+
+&emsp;
+
+## Diagram
+
+![Diagram](./images/diagram.drawio.png)
+
+## Required
+
+- [AWS CLI](https://aws.amazon.com/cli) - v2 or later
+- [AWS CDK](https://aws.amazon.com/jp/cdk) - v2 or later
+- [Node.js](https://nodejs.org/ja) - v16 or later
+- [TypeScript](https://www.typescriptlang.org/) - v4 or later
+
+## Getting Started
+
+1. Clone this repository.
+
+```shell
+$ cd ~ && git clone https://github.com/tsukuboshi/cdk-microservices-template
+```
+
+2. Install npm packages.
+
+```
+$ npm install
+```
+
+3. If this is your first CDK deployment, run this command only once.
+
+```
+$ cdk bootstrap
+```
+
+4. Deploy CDK Code.
+
+
+```
+$ cdk deploy
+```
---
+++ lib/cdk-microservices-template-stack.ts
@@ -0,0 +1,94 @@
+import { RemovalPolicy, ScopedAws, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import { DockerImageAsset, Platform } from "aws-cdk-lib/aws-ecr-assets";
+import * as ecrdeploy from "cdk-ecr-deployment";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as path from "path";
+
+export class CdkMicroservicesTemplateStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Get AWS Account ID and Region
+    const { accountId, region } = new ScopedAws(this);
+
+    // Set Resource Name
+    const resourceName = "test";
+
+    // Create ECR Repository
+    const ecrRepository = new ecr.Repository(this, "EcrRepo", {
+      repositoryName: `${resourceName}-ecr-repo`,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteImages: true,
+    });
+
+    // Create Docker Image Asset
+    const dockerImageAsset = new DockerImageAsset(this, "DockerImageAsset", {
+      directory: path.join(__dirname, "..", "app"),
+      platform: Platform.LINUX_AMD64,
+    });
+
+    // Deploy Docker Image to ECR Repository
+    new ecrdeploy.ECRDeployment(this, "DeployDockerImage", {
+      src: new ecrdeploy.DockerImageName(dockerImageAsset.imageUri),
+      dest: new ecrdeploy.DockerImageName(
+        `${accountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepository.repositoryName}:latest`
+      ),
+    });
+
+    // Create VPC and Subnet
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      vpcName: `${resourceName}-vpc`,
+      maxAzs: 2,
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/20"),
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: `${resourceName}-public`,
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    // Create ECS Cluster
+    const cluster = new ecs.Cluster(this, "EcsCluster", {
+      clusterName: `${resourceName}-cluster`,
+      vpc: vpc,
+    });
+
+    // Create CloudWatch Log Group
+    const logGroup = new logs.LogGroup(this, "LogGroup", {
+      logGroupName: `/aws/ecs/${resourceName}`,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // Create ALB and ECS Fargate Service
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "FargateService",
+      {
+        loadBalancerName: `${resourceName}-lb`,
+        publicLoadBalancer: true,
+        cluster: cluster,
+        serviceName: `${resourceName}-service`,
+        cpu: 256,
+        desiredCount: 2,
+        memoryLimitMiB: 512,
+        assignPublicIp: true,
+        taskSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+        taskImageOptions: {
+          family: `${resourceName}-taskdef`,
+          image: ecs.ContainerImage.fromEcrRepository(ecrRepository, "latest"),
+          logDriver: new ecs.AwsLogDriver({
+            streamPrefix: `container`,
+            logGroup: logGroup,
+          }),
+        },
+      }
+    );
+  }
+}
